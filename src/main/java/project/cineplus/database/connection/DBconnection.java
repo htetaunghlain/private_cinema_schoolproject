e025026250a409c7ac0f0d3bf701d20f9efc4b54
Htet Aung Hlaing <89923126+htetaunghlain@users.noreply.github.com>
2024-12-05 09:23:55 +0630

    Delete src/main/java/project/cineplus directory

--- src/main/java/project/cineplus/database/connection/DBconnection.java
+++
@@ -1,5 +0,0 @@
-package project.cineplus.database.connection;
-
-public class DBconnection {
-//        private static databaseSettings dbs = databaSettings.getInstance();
-}
--- src/main/java/project/cineplus/esjtse.java
+++
@@ -1,5 +0,0 @@
-package project.cineplus;
-
-public class esjtse {
-
-}
